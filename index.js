0b900d8b251ba10347be1a5a91ccb9e85f4501f0
champ1213 <82665513+champ1213@users.noreply.github.com>
2021-09-23 18:26:34 -0700

    theuselesswebsite original files

---
+++ index.js
@@ -0,0 +1,40 @@
+
+function cll() {
+    var button = document.getElementById("hi");
+    buttonvalue = button.value;
+    alert(buttonvalue + " clicked me")
+    
+
+};
+function cee() {
+    var buttonn = document.getElementById("text");
+    buttonvaluee = buttonn.value;
+    //alert(buttonvaluee)
+    if(buttonvaluee == "euwain") {
+        alert("hi " + buttonvaluee + " you have access");
+        return true; 
+    };
+    if(buttonvaluee == "zak") {
+        alert("hi " + buttonvaluee + " you have access");
+        return true; 
+    };
+    if(buttonvaluee == "no name bob") {
+        alert("hi " + buttonvaluee + " you have access");
+        return true; 
+    };
+    if(buttonvaluee == "scout") {
+        alert("hi " + buttonvaluee + " you have access");
+        return true; 
+    };
+    if(buttonvaluee == "cookie") {
+        alert("hi " + buttonvaluee + " you have access");
+        return true; 
+    };
+    alert("Your name was not on the list sorry");
+    alert("if know me and you want access please request")
+    return false;
+    
+
+    
+
+};
